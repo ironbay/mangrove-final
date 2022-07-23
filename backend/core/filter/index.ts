6aa6405c589a63ecc0f77d05166676921e04f357
Alan Rice <mail@alanrice.me>
2022-07-23 13:06:16 -0400

    reorg filter types

--- backend/core/filter/index.ts
+++ backend/core/filter/index.ts
@@ -1,7 +1,7 @@
-import { Entity } from "electrodb";
+import { Entity, EntityItem } from "electrodb";
 import { Dynamo } from "../dynamo";
 
-export const NumberFilterEntity = new Entity(
+export const FilterEntity = new Entity(
   {
     model: {
       entity: "NumberFilter",
@@ -14,15 +14,15 @@ export const NumberFilterEntity = new Entity(
         required: true,
         readOnly: true,
       },
-      sourceID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
       pipeID: {
         type: "string",
         required: true,
         readOnly: true,
+      },
+      filterKind: {
+        type: "string",
+        required: true,
+        readOnly: false,
       },
       op: {
         type: "string",
@@ -30,7 +30,7 @@ export const NumberFilterEntity = new Entity(
         readOnly: false,
       },
       value: {
-        type: "number",
+        type: "any",
         required: true,
         readOnly: false,
       },
@@ -45,268 +45,28 @@ export const NumberFilterEntity = new Entity(
         sk: {
           field: "sk",
           composite: [],
-        },
-      },
-      source: {
-        collection: "sources",
-        index: "gsi1",
-        pk: {
-          field: "gsi1pk",
-          composite: ["sourceID"],
-        },
-        sk: {
-          field: "gsi1sk",
-          composite: ["filterID"],
         },
       },
       pipe: {
-        collection: "pipes",
-        index: "gsi2",
+        index: "gsi1",
         pk: {
-          field: "gsi2pk",
+          field: "gsi1pk",
           composite: ["pipeID"],
         },
         sk: {
-          field: "gsi2pk",
-          composite: ["sourceID", "filterID"],
-        },
-      },
-    },
-  },
-  Dynamo.Configuration
-);
-
-export const TextFilterEntity = new Entity(
-  {
-    model: {
-      entity: "TextFilter",
-      version: "1",
-      service: "mangrove",
-    },
-    attributes: {
-      filterID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      sourceID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      pipeID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      op: {
-        type: "string",
-        required: true,
-        readOnly: false,
-      },
-      value: {
-        type: "string",
-        required: true,
-        readOnly: false,
-      },
-    },
-    indexes: {
-      filter: {
-        collection: "filters",
-        pk: {
-          field: "pk",
-          composite: ["filterID"],
-        },
-        sk: {
-          field: "sk",
+          field: "gsi1sk",
           composite: [],
         },
       },
-      source: {
-        collection: "sources",
-        index: "gsi1",
-        pk: {
-          field: "gsi1pk",
-          composite: ["sourceID"],
-        },
-        sk: {
-          field: "gsi1sk",
-          composite: ["filterID"],
-        },
-      },
-      assigned: {
-        collection: "pipes",
-        index: "gsi1",
-        pk: {
-          field: "gsi1pk",
-          composite: ["pipeID"],
-        },
-        sk: {
-          field: "gsi1sk",
-          composite: ["sourceID", "filterID"],
-        },
-      },
     },
   },
   Dynamo.Configuration
 );
 
-export const TextContainsFilterEntity = new Entity(
-  {
-    model: {
-      entity: "TextFilter",
-      version: "1",
-      service: "mangrove",
-    },
-    attributes: {
-      filterID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      sourceID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      pipeID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      op: {
-        type: "string",
-        required: true,
-        readOnly: false,
-      },
-      value: {
-        type: "list",
-        items: {
-          type: "string",
-        },
-        required: true,
-        readOnly: false,
-      },
-    },
-    indexes: {
-      filter: {
-        collection: "filters",
-        pk: {
-          field: "pk",
-          composite: ["filterID"],
-        },
-        sk: {
-          field: "sk",
-          composite: [],
-        },
-      },
-      source: {
-        collection: "sources",
-        index: "gsi1",
-        pk: {
-          field: "gsi1pk",
-          composite: ["sourceID"],
-        },
-        sk: {
-          field: "gsi1sk",
-          composite: ["filterID"],
-        },
-      },
-      assigned: {
-        collection: "pipes",
-        index: "gsi1",
-        pk: {
-          field: "gsi1pk",
-          composite: ["pipeID"],
-        },
-        sk: {
-          field: "gsi1sk",
-          composite: ["sourceID", "filterID"],
-        },
-      },
-    },
-  },
-  Dynamo.Configuration
-);
+export type FilterType = EntityItem<typeof FilterEntity>;
 
-export const SourceEntity = new Entity(
-  {
-    model: {
-      entity: "Source",
-      version: "1",
-      service: "mangrove",
-    },
-    attributes: {
-      sourceID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      pipeID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      kind: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      accountID: {
-        type: "string",
-        requried: true,
-        readOnly: false,
-      },
-    },
-    indexes: {
-      source: {
-        collection: "sources",
-        pk: {
-          field: "pk",
-          composite: ["sourceID"],
-        },
-        sk: {
-          field: "sk",
-          composite: [],
-        },
-      },
-      assigned: {
-        collection: "pipes",
-        index: "gsi1",
-        pk: {
-          field: "gsi1pk",
-          composite: ["pipeID"],
-        },
-        sk: {
-          field: "gsi1sk",
-          composite: ["sourceID"],
-        },
-      },
-    },
-  },
-  Dynamo.Configuration
-);
+export async function forPipe(pipeID: string) {
+  return FilterEntity.query.pipe({ pipeID }).go();
+}
 
-type NumberFilter<T extends string> = {
-  id: string;
-  sourceID: string;
-  kind: T;
-  op: "eq" | "lt" | "gt";
-  value: number;
-};
-
-type TextFilter<T extends string> =
-  | {
-      id: string;
-      sourceID: string;
-      kind: T;
-      op: "eq" | "contains";
-      value: string;
-    }
-  | {
-      id: string;
-      sourceID: string;
-      kind: T;
-      op: "in";
-      value: string[];
-    };
+export * as Filter from ".";
