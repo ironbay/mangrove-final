a4d5705ad33a886866c66d70a8a87ffb367069e5
Alan Rice <mail@alanrice.me>
2022-09-20 18:52:44 -0400

    restructure source type

--- backend/functions/graphql/types/pipe.ts
+++ backend/functions/graphql/types/pipe.ts
@@ -1,65 +1,54 @@
-import { builder } from "../builder";
-import { Pipe, PipeEntity } from "@mangrove/core/pipe";
-import { Source } from "@mangrove/core/source";
+import { builder } from '../builder'
+import { Pipe, PipeEntity } from '@mangrove/core/pipe'
+import { Source } from '@mangrove/core/source'
+import { PlaidSourceType } from './source'
 
-export const PipeType = builder.objectRef<Pipe.PipeEntityType>("Pipe");
+export const PipeType = builder.objectRef<Pipe.PipeEntityType>('Pipe')
 
 // homepage list implementation
 // want to get the source logo but not query all the accounts for a plaid connection!!!
 PipeType.implement({
-  fields: t => ({
-    id: t.exposeID("pipeID"),
-    name: t.exposeString("name"),
-    enabled: t.exposeBoolean("enabled"),
-    plaidSources: t.field({
-      type: [PlaidSourceType],
-      resolve: async pipe => Source.Plaid.forPipe(pipe.pipeID),
+    fields: (t) => ({
+        id: t.exposeID('pipeID'),
+        name: t.exposeString('name'),
+        enabled: t.exposeBoolean('enabled'),
+        plaidSources: t.field({
+            type: [PlaidSourceType],
+            resolve: async (pipe) => Source.Plaid.forPipe(pipe.pipeID),
+        }),
+        // sources: t.field({
+        //   type: [SourceType],
+        //   resolve: async parent => Source.forPipe(parent.pipeID),
+        // }),
+        // filters: t.field({
+        //   type: [FilterType],
+        //   resolve: parent => Filter.forPipe(parent.pipeID),
+        // }),
+        // destinations: t.field({
+        //   type: [SlackDestinationType],
+        //   resolve: parent => Destination.forPipe(parent.pipeID),
+        // }),
     }),
-    // sources: t.field({
-    //   type: [SourceType],
-    //   resolve: async parent => Source.forPipe(parent.pipeID),
-    // }),
-    // filters: t.field({
-    //   type: [FilterType],
-    //   resolve: parent => Filter.forPipe(parent.pipeID),
-    // }),
-    // destinations: t.field({
-    //   type: [SlackDestinationType],
-    //   resolve: parent => Destination.forPipe(parent.pipeID),
-    // }),
-  }),
-});
+})
 
-export const PlaidSourceType = builder
-  .objectRef<Source.Plaid.PlaidSourceEntityType>("PlaidSource")
-  .implement({
-    fields: t => ({
-      id: t.exposeID("sourceID"),
-      instLogo: t.exposeString("instLogo"),
-      instName: t.exposeString("instName"),
-      accountName: t.exposeString("accountName"),
-      accountKind: t.exposeString("accountKind"),
+builder.queryFields((t) => ({
+    pipes: t.field({
+        type: [PipeType],
+        args: {
+            userID: t.arg.string({ required: true }),
+        },
+        nullable: true,
+        resolve: async (_parent, args) => {
+            return Pipe.forUser(args.userID)
+        },
     }),
-  });
-
-builder.queryFields(t => ({
-  pipes: t.field({
-    type: [PipeType],
-    args: {
-      userID: t.arg.string({ required: true }),
-    },
-    nullable: true,
-    resolve: async (_parent, args) => {
-      return Pipe.forUser(args.userID);
-    },
-  }),
-  pipe: t.field({
-    type: PipeType,
-    args: {
-      pipeID: t.arg.string({ required: true }),
-    },
-    resolve: async (_parent, args) => {
-      return Pipe.fromID(args.pipeID);
-    },
-  }),
-}));
+    pipe: t.field({
+        type: PipeType,
+        args: {
+            pipeID: t.arg.string({ required: true }),
+        },
+        resolve: async (_parent, args) => {
+            return Pipe.fromID(args.pipeID)
+        },
+    }),
+}))
