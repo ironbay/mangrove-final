ec164fad4d52cd18765ae28b6d27fd96c9ea18b9
Alan Rice <mail@alanrice.me>
2022-09-27 22:46:12 -0400

    troubleshoot

---
+++ sst.config.mjs
@@ -0,0 +1,6 @@
+export default {
+  name: "mangrove",
+  region: "us-east-1",
+  main: "stacks/index.ts",
+  profile: "ironbay-dev",
+};
--- stacks/index.ts
+++ stacks/index.ts
@@ -9,6 +9,9 @@ export default function main(app: App) {
   app.setDefaultFunctionProps({
     runtime: "nodejs16.x",
     srcPath: "backend",
+    bundle: {
+      format: "esm",
+    },
   });
-  app.stack(Database).stack(Bus).stack(Cognito).stack(Api).stack(Web);
+  app.stack(Database).stack(Bus).stack(Api).stack(Web);
 }
