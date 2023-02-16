035e32e6612ab940420f402519ef7877bddc6e32
Alan Rice <mail@alanrice.me>
2023-02-16 11:18:06 -0500

    Pipe dynamo schema

--- backend/core/pipe/index.ts
+++ backend/core/pipe/index.ts
@@ -1,80 +1,55 @@
-import { Entity, EntityItem } from "electrodb";
-import { FilterNode } from "kysely";
-import { Dynamo, MangroveService } from "../dynamo";
-export * as Filter from "./filter";
-export * as Pipe from "./index";
+import { Entity } from "electrodb"
 
-export type PipeEntityType = EntityItem<typeof PipeEntity>;
-export const PipeEntity = new Entity(
-  {
-    model: {
-      entity: "Pipe",
-      version: "1",
-      service: "mangrove",
+const PipeEntity = new Entity({
+  model: {
+    entity: "Pipe",
+    version: "1",
+    service: "mangrove",
+  },
+  attributes: {
+    pipeID: {
+      type: "string",
+      required: true,
     },
-    attributes: {
-      pipeID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      userID: {
-        type: "string",
-        required: true,
-        readOnly: true,
-      },
-      name: {
-        type: "string",
-        required: true,
-        readOnly: false,
-      },
-      enabled: {
-        type: "boolean",
-        required: true,
-        readOnly: false,
+    userID: {
+      type: "string",
+      required: true,
+    },
+    status: {
+      type: "string",
+      required: true,
+    },
+    name: {
+      type: "string",
+      required: true,
+    },
+  },
+  indexes: {
+    primary: {
+      pk: {
+        field: "pk",
+        composite: ["pipeID"],
       },
     },
-    indexes: {
-      pipe: {
-        collection: "pipes",
-        pk: {
-          field: "pk",
-          composite: ["pipeID"],
-        },
-        sk: {
-          field: "sk",
-          composite: ["userID"],
-        },
+    pipeLookup: {
+      pk: {
+        field: "gsi2pk",
+        composite: ["pipeID"],
       },
-      user: {
-        index: "gsi1",
-        pk: {
-          field: "gsi1pk",
-          composite: ["userID"],
-        },
-        sk: {
-          field: "gsi1sk",
-          composite: ["pipeID"],
-        },
+      sk: {
+        field: "gsi2sk",
+        composite: ["pipeID"],
+      },
+    },
+    byUserID: {
+      pk: {
+        field: "gsi3pk",
+        composite: ["userID"],
+      },
+      sk: {
+        field: "gsi3sk",
+        composite: ["pipeID"],
       },
     },
   },
-  Dynamo.Configuration
-);
-
-export async function forUser(userID: string): Promise<PipeEntityType[]> {
-  return [
-    {
-      pipeID: "pipe1",
-      userID: "user123",
-      enabled: true,
-      name: "my sweet pipe",
-    },
-  ];
-  //   return PipeEntity.query.user({ userID }).go();
-}
-
-export async function fromID(pipeID: string) {
-  const [pipe] = await PipeEntity.query.pipe({ pipeID }).go();
-  return pipe;
-}
+})
