dcc522d5de523282040afbc6affaae094aacb76a
Alan Rice <mail@alanrice.me>
2023-02-19 12:55:17 -0500

    node imports

--- backend/core/connection/slack.ts
+++ backend/core/connection/slack.ts
@@ -1,9 +1,9 @@
-import { WebClient as Client } from "@slack/web-api";
-import { Entity, EntityItem } from "electrodb";
-import { Dynamo } from "../dynamo";
-import { config, Config } from "@serverless-stack/node/config";
-import { ulid } from "ulid";
-import { Response } from "aws-sdk";
+import { WebClient as Client } from "@slack/web-api"
+import { Entity, EntityItem } from "electrodb"
+import { Dynamo } from "../dynamo"
+import { Config } from "sst/node/config"
+import { ulid } from "ulid"
+import { Response } from "aws-sdk"
 
 export const SlackConnectionEntity = new Entity(
   {
@@ -65,59 +65,59 @@ export const SlackConnectionEntity = new Entity(
     },
   },
   Dynamo.Configuration
-);
+)
 
 function client(accessToken?: string) {
-  return new Client(accessToken);
+  return new Client(accessToken)
 }
 
 async function clientFromID(connectionID: string) {
-  const conn = await fromID(connectionID);
-  return client(conn.accessToken);
+  const conn = await fromID(connectionID)
+  return client(conn.accessToken)
 }
 
 export async function forUser(userID: string) {
-  return SlackConnectionEntity.query.user({ userID }).go();
+  return SlackConnectionEntity.query.user({ userID }).go()
 }
 
 export async function fromID(connectionID: string) {
   const [conn] = await SlackConnectionEntity.query
     .connection({ connectionID })
-    .go();
+    .go()
 
-  return conn;
+  return conn
 }
 
 export async function teamForDestination(connID: string, teamID: string) {
-  const client = await clientFromID(connID);
-  const resp = await client.team.info({ team: teamID });
+  const client = await clientFromID(connID)
+  const resp = await client.team.info({ team: teamID })
 
   return {
     id: resp.team!.id!,
     name: resp.team!.name!,
     icon: resp.team!.icon?.image_230,
-  };
+  }
 }
 
 export async function channelForDestination(connID: string, channelID: string) {
-  const client = await clientFromID(connID);
-  const resp = await client.conversations.info({ channel: channelID });
+  const client = await clientFromID(connID)
+  const resp = await client.conversations.info({ channel: channelID })
 
   return {
     id: resp.channel!.id!,
     name: resp.channel!.name!,
-  };
+  }
 }
 
 export async function channels(connectionID: string) {
-  const connection = await fromID(connectionID);
-  const resp = await client(connection.accessToken).conversations.list();
-  if (!resp.channels) return [];
+  const connection = await fromID(connectionID)
+  const resp = await client(connection.accessToken).conversations.list()
+  if (!resp.channels) return []
 
-  return resp.channels.map(channel => ({
+  return resp.channels.map((channel) => ({
     id: channel.id!,
     name: channel.name!,
-  }));
+  }))
 }
 
 export async function create(userID: string, code: string) {
@@ -125,7 +125,7 @@ export async function create(userID: string, code: string) {
     code,
     client_id: Config.SLACK_CLIENT_ID,
     client_secret: Config.SLACK_CLIENT_SECRET,
-  });
+  })
 
   const created = await SlackConnectionEntity.create({
     connectionID: ulid(),
@@ -133,13 +133,11 @@ export async function create(userID: string, code: string) {
     accessToken: resp.access_token!,
     teamID: resp.team!.id!,
     teamName: resp.team!.name!,
-  }).go();
+  }).go()
 
-  return fromID(created.connectionID);
+  return fromID(created.connectionID)
 }
 
-export type SlackConnectionEntityType = EntityItem<
-  typeof SlackConnectionEntity
->;
+export type SlackConnectionEntityType = EntityItem<typeof SlackConnectionEntity>
 
-export * as Connection from ".";
+export * as Connection from "."
--- backend/functions/graphql/graphql.ts
+++ backend/functions/graphql/graphql.ts
@@ -1,6 +1,6 @@
-import { schema } from "./schema";
-import { GraphQLHandler } from "@serverless-stack/node/graphql";
+import { schema } from "./schema"
+import { GraphQLHandler } from "sst/node/graphql"
 
 export const handler = GraphQLHandler({
   schema,
-});
+})
