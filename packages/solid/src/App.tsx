b15eb502c446f57db1df7714642536ed77d999a3
Alan Rice <mail@alanrice.me>
2023-04-14 12:19:51 -0400

    hello world solidjs

--- packages/solid/src/App.tsx
+++ packages/solid/src/App.tsx
@@ -1,9 +1,34 @@
-import type { Component } from 'solid-js';
+import {
+  createSignal,
+  onCleanup,
+  type Component,
+  Show,
+  For,
+  Switch,
+  Match,
+} from "solid-js"
 
-const App: Component = () => {
+function App() {
+  let interval: ReturnType<typeof setInterval> = null
+  const [count, setCount] = createSignal(0)
+
+  function startInterval() {
+    interval = setInterval(() => setCount(count() + 1), 1000)
+  }
+
+  onCleanup(() => clearInterval(interval))
+
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-  );
-};
+    <div>
+      <div>count: {count()}</div>
+      <div onClick={startInterval}>start timer</div>
+      <Switch fallback={<div>nothing yet...</div>}>
+        <Match when={count() < 5}>Less than 5</Match>
+        <Match when={count() < 10}>Less than 10</Match>
+        <Match when={count() < 15}>Less than 15</Match>
+      </Switch>
+    </div>
+  )
+}
 
-export default App;
+export default App
