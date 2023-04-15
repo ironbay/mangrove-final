e765ba4650f46b64a5ce325886d157999412991d
Alan Rice <mail@alanrice.me>
2023-04-15 12:29:59 -0400

    setup macaron

--- packages/solid/vite.config.ts
+++ packages/solid/vite.config.ts
@@ -1,12 +1,33 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite"
+import solidPlugin from "vite-plugin-solid"
+import { macaronVitePlugin } from "@macaron-css/vite"
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [macaronVitePlugin(), solidPlugin()],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
-});
+})
+// import { macaronVitePlugin } from "@macaron-css/vite"
+// import { defineConfig } from "vite"
+// import solidPlugin from "vite-plugin-solid"
+// import { macaronVitePlugin } from "@macaron-css/vite"
+// import path from "path"
+
+// export default defineConfig({
+//   plugins: [macaronVitePlugin() as any, solidPlugin()],
+//   server: {
+//     port: 3000,
+//   },
+//   build: {
+//     target: "esnext",
+//   },
+//   resolve: {
+//     alias: {
+//       "@goodvisit/ui": path.resolve(__dirname, "./src/ui"),
+//     },
+//   },
+// })
